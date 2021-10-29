6a5e576845b38641202743b4f0002109d2a3e8d4
Shubham Pandey <shubham.pandey@fangame.live>
2021-10-29 10:40:37 +0530

    minor refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './reducers/Store'
+import store from './redux-store/Store';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
