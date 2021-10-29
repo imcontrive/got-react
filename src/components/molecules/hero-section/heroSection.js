0dd47e0b614c90cb002c8c7a0d5cab887234a4ce
Shubham Pandey <shubham.pandey@fangame.live>
2021-10-29 10:39:19 +0530

    added atoms along with herosection

---
+++ src/components/molecules/hero-section/heroSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './hero-section.css'
+
+export default function HeroSection() {
+    return (
+        <section id="hero">
+            <div class="header">
+                <h1 class="h1style">
+                    <i class="fab fa-fort-awesome"></i>  Game Of Thrones  <i class="fab fa-fort-awesome"></i>
+                </h1>
+                <p class="p1style">All the data from the universe of Ice And Fire you've ever wanted.</p>
+            </div>
+        </section>
+    )
+}
