c3bacfdd280c93c3af08008f19813e809ea544d8
Shubham Pandey <shubham.pandey@fangame.live>
2021-11-01 12:03:20 +0530

    added books cards UI and some featues

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,29 +1,94 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import { getAllBooks, getAllCharacters, getAllHomes } from '../../businessLogic'
+import BookCard from '../molecules/book-card/bookCard';
 import HeroSection from '../molecules/hero-section/heroSection';
 
-export default class Home extends Component {
-  state={
-    activeBtn:'btn-get-books',
+const btns = [ {
+  iconClassName: 'fas fa-book',
+  content: 'Show All Books',
+},
+{
+  iconClassName: 'fas fa-home',
+  content: 'Show All Houses',
+},
+{
+  iconClassName: 'fas fa-user',
+  content: 'Show All Characters',
+} ]
 
+class Home extends Component {
+  state = {
+    activeBtnIndex: 0,
   }
-  
+
   componentDidMount() {
     getAllBooks();
     getAllHomes();
     getAllCharacters();
   }
 
+  updateActiveBtnIndex = (index) => {
+    this.setState({ activeBtnIndex: index })
+  }
+
+  renderView = (books) => {
+    const { activeBtnIndex } = this.state;
+    if (activeBtnIndex === 1) {
+      return <h2 className="text-center">WIP: houses not found</h2>
+    } else if (activeBtnIndex === 2) {
+      return <h2 className="text-center">WIP:characters not found</h2>
+    } else {
+      return (
+        <div className="books-wrapper">
+          {
+            books ? books.map((book, index) =>
+              <BookCard data={book} index={index} />
+            ) : <h2 className="text-center">books not found</h2>
+          }
+        </div>
+      )
+    }
+  }
+
   render() {
+    const { activeBtnIndex } = this.state;
+    const { books, houses, characters } = this.props;
+
     return (
-      <div>
+      <div style={{paddingBottom:"3rem"}}>
         <HeroSection />
         <div className='data-section'>
-          <button className='btn btn-get-books' ><i class="fas fa-book"></i> Show All Books</button>
-          <button className='btn btn-get-homes' ><i class="fas fa-home"></i> Show All Houses</button>
-          <button className='btn btn-get-users' ><i class="fas fa-user"></i> Show All Characters</button>
+          {
+            btns.map((btn, index) =>
+              <button
+                className={activeBtnIndex === index ? 'btn active-btn' : 'btn'}
+                onClick={() => this.updateActiveBtnIndex(index)}>
+                <i class={btn.iconClassName}></i>{" " + btn.content + " "}
+              </button>
+            )
+          }
         </div>
+
+
+        <div class="section-heading">
+          <span class="section-title">
+            {activeBtnIndex === 1 ? "Houses List" : activeBtnIndex === 2 ? "Characters List" : "Books List"}
+          </span>
+        </div>
+
+        {this.renderView(books)}
       </div>
     )
   }
 }
+
+
+const mapStateToProps = (state) => {
+  return {
+    books: state.mainReducer.books,
+    houses: state.mainReducer.houses,
+    characters: state.mainReducer.characters
+  }
+}
+export default connect(mapStateToProps)(Home)
