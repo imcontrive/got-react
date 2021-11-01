b5642bae8041f3366ec474c448f9e8b68a4b040e
Shubham Pandey <shubham.pandey@fangame.live>
2021-11-01 12:02:43 +0530

    added cards for characters, houses

---
+++ src/components/molecules/char-card/charCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './char-card.css'
+
+export default function CharCard({
+    url,
+    name,
+    gender,
+    culture,
+    born,
+    died,
+    aliases,//[]
+    titles, //[]
+    books, //[apis]
+    povBooks, //[]
+    tvSeries, //[]
+    playedBy,//[]
+}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/molecules/house-card/houseCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './house-card.css'
+
+export default function HouseCard({
+    url,
+    name,
+    region,
+    coatOfArms,
+    words,
+    title, //[]
+    seats, //[]
+    currentLord,
+    ancestralWeapons,//[]
+    cadetBranches,//[]
+    swornMembers //[]
+}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
