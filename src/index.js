72d635d2288e0eff9030d88cb49e9fab24605741
Shubham Pandey <shubham.pandey@fangame.live>
2021-11-01 11:59:07 +0530

    changes css routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './redux-store/Store';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 
